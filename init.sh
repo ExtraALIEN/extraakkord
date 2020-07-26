054d6a37e475e72379724e0b3bc32cf24fedab6b
ExtraALIEN <ExtraAlien@yandex.ru>
2020-07-26 20:33:49 +0300

    first commit

---
+++ init.sh
@@ -0,0 +1,4 @@
+sudo unlink /etc/nginx/sites-enabled/extraakkord.conf
+sudo ln -s /home/extraalien/projects/extraakkord/etc/extraakkord.conf /etc/nginx/sites-enabled
+sudo service nginx stop
+sudo service nginx start
